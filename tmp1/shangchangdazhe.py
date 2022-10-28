488a72c574481489e94a52e2c0e171b1f3821ea0
liqianhu-fafafa <18762050806@163.com>
2022-10-28 10:21:09 +0800

    add file

---
+++ tmp1/shangchangdazhe.py
@@ -0,0 +1,21 @@
+vip = raw_input("are you vip(Y/N):")
+money = float(input("input your money:"))
+anwser = 'Y'
+
+if vip == anwser:
+    if money >= 200:
+        pay = money * 0.8
+        print("0.8 off", pay)
+    elif 50 < money < 200:
+        pay = money * 0.95
+        print("0.95 off", pay)
+    else: 
+        print(money)
+        print("bu da zhe!")
+else:
+    if money >= 200:
+        pay = money * 0.95
+        print("0.95 off", pay)
+    else:
+        print(money)
+        print("bu da zhe!")
