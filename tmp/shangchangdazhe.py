2ed70b08770276ae234e7705ee1c81d3ddde9ddd
liqianhu <18762050806@163.com>
2023-01-04 16:26:19 +0800

    modify file

--- tmp/shangchangdazhe.py
+++ tmp/shangchangdazhe.py
@@ -1,22 +1,22 @@
-vip = raw_input("are you vip(Y/N):")
-money = float(input("input your money:"))
+vip = raw_input("你是VIP么(Y/N):")
+money = float(input("请输入你的购物金额:"))
 anwser = 'Y'
 
 if vip == anwser:
     if money >= 200:
         pay = money * 0.8
-        print("0.8 off", pay)
+        print("打8折", pay)
     elif 50 < money < 200:
         pay = money * 0.95
-        print("0.95 off", pay)
+        print("打95折", pay)
     else: 
         print(money)
-        print("bu da zhe!")
+        print("不打折!")
 else:
     if money >= 200:
         pay = money * 0.95
-        print("0.95 off", pay)
+        print("打95折", pay)
     else:
         print(money)
-        print("bu da zhe!")
+        print("不打折!")
 
